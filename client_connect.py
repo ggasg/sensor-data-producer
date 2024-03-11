0c15f33559ba08caa596a19945f711ec36730b03
Gaston G <gaston@ggs-svl2.lan>
2024-03-10 23:02:09 -0400

    Sending hardcoded messages to mqtt

--- client_connect.py
+++ client_connect.py
@@ -1,43 +1,40 @@
 from paho.mqtt import client as mqtt
 import time
+import datetime
 import os
 from dotenv import load_dotenv
 
-# overriding on_publish callback
-def on_publish(client, userdata, mid, reason_code, properties):
-    try:
-        userdata.remove(mid)
-    except KeyError:
-        print('Chachi messed up just this time')
+class MQTTClient:
 
-load_dotenv()
+    # overriding on_publish callback
+    def on_publish(self, client, userdata, mid, reason_code, properties):
+        try:
+            userdata.remove(mid)
+        except KeyError:
+            print('Chachi messed up just this time')
 
-broker = os.getenv("BROKER_HOST")
-unacked_publish = set()
+    def generate_timestamp_element(self):
+        return f"\"timestamp\":\"{str(datetime.datetime.now())}\""
 
-client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-client.on_publish = on_publish
-client.user_data_set(unacked_publish)
+    def __init__(self) -> None:
+        load_dotenv()
+        broker = os.getenv("BROKER_HOST")
+        self.topic = os.getenv("TEMP_SENSOR_TOPIC")
+        self.unacked_publish = set()
+        self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+        self.client.on_publish = self.on_publish
+        self.client.user_data_set(self.unacked_publish)
+        print('Connecting to MQTT...', broker)
+        self.client.username = os.getenv("BROKER_USR")
+        self.client.password = os.getenv("BROKER_PWD")
+        will_message = f"{self.generate_timestamp_element()}, \"Client Disconnect\""
+        self.client.will_set(self.topic, will_message,qos=1, retain=False)
+        self.client.connect(broker)
 
-print('Connecting to MQTT...', broker)
-client.username = os.getenv("BROKER_USR")
-client.password = os.getenv("BROKER_PWD")
-client.connect(broker)
+        self.client.loop_start()
 
-client.loop_start()
-
-msg_info = client.publish('paho/test/topic', "my message is none", qos=1)
-unacked_publish.add(msg_info.mid)
-
-msg_info2 = client.publish('paho/test/topic', "my message is none 2", qos=1)
-unacked_publish.add(msg_info2.mid)
-
-
-while len(unacked_publish):
-    time.sleep(1)
-
-msg_info.wait_for_publish()
-msg_info2.wait_for_publish()
-
-client.disconnect()
-client.loop_stop()
+    def publish_to_central(self, payload):
+        reading_message = f"\"reading\": {payload}"
+        msg_info = self.client.publish(self.topic, f"{{{self.generate_timestamp_element()}, {reading_message}}}", qos=1)
+        self.unacked_publish.add(msg_info.mid)
+        msg_info.wait_for_publish()
--- main.py
+++ main.py
@@ -3,28 +3,42 @@ from threading import Thread
 from bmp180 import Sensor
 from smbus2 import SMBus
 import time
+import json
+from client_connect import MQTTClient
 
-def annoy_me(sensor, stop):
-    while (True):
-        msg = {}
-        msg['measure_timestamp'] = time.time()
-        msg['pressure'] = sensor.get_pressure()
-        msg['temperature'] = sensor.get_temperature()
+# def annoy_me(sensor, stop):
+#     while (True):
+#         msg = {}
+#         msg['measure_timestamp'] = time.time()
+#         msg['pressure'] = sensor.get_pressure()
+#         msg['temperature'] = sensor.get_temperature()
+#         time.sleep(5)
+#         if stop():
+#             print('Ok now leaving')
+#             break
+#     print('Thread signing off')
+
+def read_in_thread(mqtt, stop):
+    while True:
+        reading = {}
+        reading["temp"] = 10.30
+        reading["pressure"] = 1.2
+        mqtt.publish_to_central(json.dumps(reading))
         time.sleep(5)
         if stop():
             print('Ok now leaving')
             break
-    print('Thread signing off')
+    print('Thread signing off...')
 
 
-# TODO - How is Sensor supposed to be instantiated?
-def main() -> None:
+def main():
     stop_threads = False
-    bus = SMBus(1)
-    reading = Sensor(bus)
-    process = Thread(target=annoy_me, args=(reading, lambda: stop_threads))
+    # bus = SMBus(1)
+    # reading = Sensor(bus)
+    mqtt_client = MQTTClient()
+    process = Thread(target=read_in_thread, args=(mqtt_client, lambda: stop_threads))
     try:
-        print('At any time, submit q to exit program, CTRL-C to abort ungracefully yes yes')
+        print('At any time, submit q to exit program, CTRL-C to abort')
         process.start()
         while True:
             choice = input()
@@ -32,10 +46,12 @@ def main() -> None:
                 stop_threads = True
                 process.join()
                 break
-            print('bleuuuurrrgh')
     except KeyboardInterrupt:
         print('Aborted')
         sys.exit(1)
+    finally:
+        mqtt_client.client.disconnect()
+        mqtt_client.client.loop_stop()
 
 if __name__ == '__main__':
     main()
