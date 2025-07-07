c2258b6db9f3112ae11a8175aeeca4268e5fcdbf
gaston <gaston@mac.lan>
2025-07-07 19:19:05 -0400

    Added TLS settings and changed QOS to 2

--- client_connect.py
+++ client_connect.py
@@ -1,4 +1,5 @@
 from paho.mqtt import client as mqtt
+import ssl
 import time
 import os
 from dotenv import load_dotenv
@@ -18,22 +19,24 @@ class MQTTClient:
     def __init__(self) -> None:
         load_dotenv()
         broker = os.getenv("BROKER_HOST")
+        password = os.getenv("BROKER_PWD")
         self.topic = os.getenv("TEMP_SENSOR_TOPIC")
         self.unacked_publish = set()
         self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
         self.client.on_publish = self.on_publish
         self.client.user_data_set(self.unacked_publish)
+        ssl_settings = ssl.SSLContext(ssl.PROTOCOL_TLS)
+        self.client.tls_set_context(ssl_settings)
         print('Connecting to MQTT...', broker)
-        self.client.username = os.getenv("BROKER_USR")
-        self.client.password = os.getenv("BROKER_PWD")
+        self.client.username_pw_set(os.getenv("BROKER_USR"), os.getenv("BROKER_PWD"))
         will_message = f"{self.generate_timestamp_element()}, \"Client Disconnect\""
-        self.client.will_set(self.topic, will_message,qos=1, retain=False)
-        self.client.connect(broker)
+        self.client.will_set(self.topic, will_message, qos=2, retain=False)
+        self.client.connect(host=broker, port=8883)
 
         self.client.loop_start()
 
     def publish_to_central(self, payload):
         reading_message = f"\"reading\": {payload}"
-        msg_info = self.client.publish(self.topic, f"{{{self.generate_timestamp_element()}, {reading_message}}}", qos=1)
+        msg_info = self.client.publish(self.topic, f"{{{self.generate_timestamp_element()}, {reading_message}}}", qos=2)
         self.unacked_publish.add(msg_info.mid)
         msg_info.wait_for_publish()
